5af94eae8d9578eee7805e56df585abd1119f48f
ryan-williams <nobigdealstyle@gmail.com>
2022-11-15 22:39:54 +0000

    Deploying to gh-pages from @ neighbor-ryan/turnpiketrap@07e1064d3d67b1a9dc274a6bd87dc3ea08faf91d 🚀

--- _next/static/tPO5kSmzaZG-TcQMWf3ie/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/346-d6f19acaa377e61d.js","static/css/e80398c391f944fa.css","static/chunks/pages/index-0c515bc698be811b.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
